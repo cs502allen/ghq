f212ee607f0e2c31a77c8bcebe95b00e7f520098
Aidan Cunniffe <acunniffe@gmail.com>
2024-11-04 09:05:48 -0500

    clean up imports

--- src/game/tests/capture-logic.test.ts
+++ src/game/tests/capture-logic.test.ts
@@ -1,14 +1,6 @@
 import { describe, expect, it } from "@jest/globals";
 import { GHQState } from "@/game/engine";
-import { movesForActivePiece } from "@/game/move-logic";
-import {
-  Blue,
-  initialBoardSetup,
-  initialBoardSetupWithAnAirborneBack,
-  initialBoardSetupWithAnAirborneNotBack,
-  initialBoardSetupWithAnArmored,
-  Red,
-} from "@/game/tests/test-boards";
+import { Blue, Red } from "@/game/tests/test-boards";
 import { captureCandidates } from "@/game/capture-logic";
 
 const BINF = Blue.INFANTRY;
