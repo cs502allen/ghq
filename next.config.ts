dd7cdde0fc4174b81278910fbd1c038d2212ef76
Aidan Cunniffe <aidan@useoptic.com>
2024-11-05 12:15:41 -0500

    wild and annoying ts issues

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
+    tsconfigPath: "./tsconfig.json",
     // @todo figure out why tsconfig doesn't exclude node modules
     ignoreBuildErrors: true,
   },
