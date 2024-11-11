8104cf3075b3e0532fa8fb139631c3a9d143b982
R. Tyler Julian <trjulian@gmail.com>
2024-11-10 23:18:18 -0500

    add bombardment to eval

--- src/game/eval.ts
+++ src/game/eval.ts
@@ -1,5 +1,6 @@
 import { isPieceArtillery } from "./board-moves";
 import { GHQState, Player } from "./engine";
+import { bombardedSquares } from "./move-logic";
 
 const unitScores: Record<string, number> = {
   INFANTRY: 1,
@@ -27,8 +28,9 @@ export function calculateEval(board: GHQState["board"]): number {
     BLUE: 0,
   };
 
-  // TODO(tyler): give points for bombarding central squares
   // TODO(tyler): give points for getting to a position where you can take a piece for free
+
+  const bombarded = bombardedSquares(board);
 
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
@@ -37,6 +39,7 @@ export function calculateEval(board: GHQState["board"]): number {
         continue;
       }
 
+      // Add positional score
       if (square.type === "AIRBORNE_INFANTRY") {
         const homeRow = square.player === "RED" ? 7 : 0;
         const distance = Math.abs(i - homeRow);
@@ -47,6 +50,14 @@ export function calculateEval(board: GHQState["board"]): number {
       } else {
         scores[square.player] +=
           (unitScores[square.type] ?? 0) + gradient[i][j];
+      }
+
+      // If a player bombards a square with an enemy piece in it, give them points
+      if (bombarded[`${i},${j}`]?.RED && square.player === "BLUE") {
+        scores.RED += 0.5;
+      }
+      if (bombarded[`${i},${j}`]?.BLUE && square.player === "RED") {
+        scores.BLUE += 0.5;
       }
     }
   }
