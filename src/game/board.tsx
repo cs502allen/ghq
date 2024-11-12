12ff92cb08dc105d3b08493ea373f38bd266839a
Aidan Cunniffe <acunniffe@gmail.com>
2024-11-12 07:59:36 -0500

    should not double render moves on live games

--- src/game/board.tsx
+++ src/game/board.tsx
@@ -149,11 +149,12 @@ export function GHQBoard({
     })
   );
 
-  const renderBoard = state.matches("replay")
-    ? ctx.currentPlayer === "0"
-      ? G.redTurnStartBoard
-      : G.blueTurnStartBoard
-    : G.board;
+  const renderBoard =
+    state.matches("replay") || state.matches("notTurn")
+      ? ctx.currentPlayer === "0"
+        ? G.redTurnStartBoard
+        : G.blueTurnStartBoard
+      : G.board;
 
   const renderMoves = state.matches("replay")
     ? G.lastPlayerMoves
