144737503d9fb089e778454961f9bd8d8e23a4c6
R. Tyler Julian <trjulian@gmail.com>
2024-11-11 20:46:17 -0500

    add loading state to /learn

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -7,17 +7,19 @@ import ReplayCapability from "@/game/ReplayCapability";
 import { BoardType, newTutorialGHQGame } from "@/game/tutorial";
 import { useSearchParams } from "next/navigation";
 import { boards } from "@/game/tutorial";
-import Image from "next/image";
 import Header from "@/components/Header";
+import { Loader2 } from "lucide-react";
 
 export default function Page() {
   const searchParams = useSearchParams();
   const [client, setClient] = useState<any | null>(null);
   const [App, setApp] = useState<any | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const boardType = searchParams.get("boardType") as BoardType;
     if (!boardType) {
+      setLoading(false);
       return;
     }
 
@@ -25,27 +27,40 @@ export default function Page() {
       game: newTutorialGHQGame({ boardType }),
       board: GHQBoard,
     });
-    setApp(() => DynamicApp);
+    setApp(() => {
+      setLoading(false);
+      return DynamicApp;
+    });
   }, [searchParams]);
 
   if (!App) {
     return (
       <div className="p-2 flex flex-col gap-4 lg:px-48">
         <Header />
-        <div className="flex flex-col gap-2 border rounded p-4 bg-slate-50">
-          <div className="text-2xl">Lessons</div>
-          <div className="flex flex-wrap gap-1">
-            {Object.keys(boards).map((boardType: string) => (
-              <a
-                key={boardType}
-                href={`/learn?boardType=${boardType}`}
-                className="py-3 px-4 bg-white border border-gray-200 rounded-lg shadow hover:shadow-md min-w-96"
-              >
-                <div className="tracking-tight text-gray-900">{boardType}</div>
-              </a>
-            ))}
+
+        {loading && (
+          <div className="flex gap-1">
+            <Loader2 className="animate-spin w-6 h-6" /> Loading...
           </div>
-        </div>
+        )}
+        {!loading && (
+          <div className="flex flex-col gap-2 border rounded p-4 bg-slate-50">
+            <div className="text-2xl">Lessons</div>
+            <div className="flex flex-wrap gap-1">
+              {Object.keys(boards).map((boardType: string) => (
+                <a
+                  key={boardType}
+                  href={`/learn?boardType=${boardType}`}
+                  className="py-3 px-4 bg-white border border-gray-200 rounded-lg shadow hover:shadow-md min-w-96"
+                >
+                  <div className="tracking-tight text-gray-900">
+                    {boardType}
+                  </div>
+                </a>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     );
   }
