3faf5b92223140042e1fff74ca60d3753decf1ec
R. Tyler Julian <trjulian@gmail.com>
2024-11-13 09:11:41 -0500

    small size squares

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -2,7 +2,7 @@ export const rows = 8;
 export const columns = 8;
 export const MOVE_SPEED_MS = 250;
 export const squareSizes = {
-  small: 65,
+  small: 45,
   large: 75,
 };
 export const pieceSizes = {
