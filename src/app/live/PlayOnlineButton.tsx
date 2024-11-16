cc3e76e33baf9d99047e15c4dfaa115f46dfca8e
R. Tyler Julian <trjulian@gmail.com>
2024-11-16 08:01:34 -0500

    rm unused files

--- src/app/live/PlayOnlineButton.tsx
+++ src/app/live/PlayOnlineButton.tsx
@@ -26,7 +26,6 @@ export function PlayOnlineButton({
       <Button onClick={playOnline} loadingText="Searching...">
         🌎 Play Online
       </Button>
-      {/* {isMatchmaking && <MatchmakingModal onCancel={cancelMatchmaking} />} */}
     </>
   );
 }
