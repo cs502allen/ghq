e653c038465bd74025fa49d2bc8e0366695c0679
R. Tyler Julian <trjulian@gmail.com>
2024-12-01 08:02:47 -0500

    bot tweaks

--- src/game/eval.ts
+++ src/game/eval.ts
@@ -1,6 +1,6 @@
 import { getAllowedMoves, isPieceArtillery } from "./board-moves";
 import { freeInfantryCaptures } from "./capture-logic";
-import { AllowedMove, GHQState, Player, ReserveFleet } from "./engine";
+import { AllowedMove, Board, Player, ReserveFleet } from "./engine";
 import { bombardedSquares } from "./move-logic";
 
 const unitScores: Record<string, number> = {
@@ -27,43 +27,43 @@ const gradient: number[][] = [
 const bombardGradient: number[][] = [
   [-0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1],
   [-0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.1],
-  [-0.1, 0.0, 0.05, 0.05, 0.05, 0.05, 0.0, -0.1],
-  [-0.1, 0.0, 0.05, 0.1, 0.1, 0.05, 0.0, -0.1],
-  [-0.1, 0.0, 0.05, 0.1, 0.1, 0.05, 0.0, -0.1],
-  [-0.1, 0.0, 0.05, 0.05, 0.05, 0.05, 0.0, -0.1],
+  [-0.05, 0.0, 0.05, 0.05, 0.05, 0.05, 0.0, -0.05],
+  [-0.05, 0.0, 0.05, 0.1, 0.1, 0.05, 0.0, -0.05],
+  [-0.05, 0.0, 0.05, 0.1, 0.1, 0.05, 0.0, -0.05],
+  [-0.05, 0.0, 0.05, 0.05, 0.05, 0.05, 0.0, -0.05],
   [-0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.1],
   [-0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1],
 ];
 
 export interface EvalBoardState {
-  board: GHQState["board"];
+  board: Board;
   redReserve: ReserveFleet;
   blueReserve: ReserveFleet;
   currentPlayerTurn: Player;
   thisTurnMoves: AllowedMove[];
 }
 
-export function calculateEval({
+function updateScoresForCaptures({
   board,
   redReserve,
   blueReserve,
   currentPlayerTurn,
   thisTurnMoves,
-}: EvalBoardState): number {
-  const scores: Record<Player, number> = {
-    RED: 0,
-    BLUE: 0,
-  };
-
+  scores,
+  calculateOpponent,
+}: EvalBoardState & {
+  scores: Record<Player, number>;
+  calculateOpponent: boolean;
+}) {
   const allowedMoves = getAllowedMoves({
     board,
     redReserve,
     blueReserve,
-    currentPlayerTurn,
+    currentPlayerTurn: calculateOpponent
+      ? getOpponent(currentPlayerTurn)
+      : currentPlayerTurn,
     thisTurnMoves,
   });
-
-  // Find a list of candidate captures
   for (const move of allowedMoves) {
     if (move.name === "Move" && move.args.length === 3) {
       const [from, , capture] = move.args;
@@ -86,11 +86,48 @@ export function calculateEval({
         continue;
       }
       scores[fromPiece.player] += captureScore;
-      scores[capturePiece.player] -= captureScore;
     }
   }
 
-  const freeCaptured = freeInfantryCaptures(board, currentPlayerTurn);
+  return scores;
+}
+
+export function calculateEval({
+  board,
+  redReserve,
+  blueReserve,
+  currentPlayerTurn,
+  thisTurnMoves,
+}: EvalBoardState): number {
+  const scores: Record<Player, number> = {
+    RED: 0,
+    BLUE: 0,
+  };
+
+  updateScoresForCaptures({
+    board,
+    redReserve,
+    blueReserve,
+    currentPlayerTurn,
+    thisTurnMoves,
+    scores,
+    calculateOpponent: false,
+  });
+
+  updateScoresForCaptures({
+    board,
+    redReserve,
+    blueReserve,
+    currentPlayerTurn,
+    thisTurnMoves,
+    scores,
+    calculateOpponent: true,
+  });
+
+  const freeCaptured = freeInfantryCaptures(
+    board,
+    getOpponent(currentPlayerTurn)
+  );
   for (const capture of freeCaptured) {
     const captured = capture.capture;
     const captureScore = unitScores[captured.piece.type];
@@ -100,7 +137,6 @@ export function calculateEval({
     }
 
     scores[captured.piece.player] -= captureScore;
-    scores[captured.piece.player === "RED" ? "BLUE" : "RED"] += captureScore;
   }
 
   const bombarded = bombardedSquares(board);
@@ -135,14 +171,12 @@ export function calculateEval({
         scores.BLUE += bombardGradient[i][j];
       }
 
-      // If a player bombards a square with an enemy piece in it, give them points
+      // If a player bombards a square with an enemy piece in it, reduce the enemy's score
       if (bombarded[`${i},${j}`]?.RED && square.player === "BLUE") {
-        scores.RED += thisSquareValue * 0.5;
-        scores.BLUE -= thisSquareValue * 0.5;
+        scores.BLUE -= thisSquareValue;
       }
       if (bombarded[`${i},${j}`]?.BLUE && square.player === "RED") {
-        scores.BLUE += thisSquareValue * 0.5;
-        scores.RED -= thisSquareValue * 0.5;
+        scores.RED -= thisSquareValue;
       }
 
       // Add points if this piece has some good captures
@@ -150,4 +184,8 @@ export function calculateEval({
   }
 
   return Math.round(100 * (scores.RED - scores.BLUE)) / 100;
+}
+
+function getOpponent(player: Player): Player {
+  return player === "RED" ? "BLUE" : "RED";
 }
