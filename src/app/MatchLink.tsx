06f0e15916bceec1d42c74fa8445491b6cd53af9
R. Tyler Julian <trjulian@gmail.com>
2025-04-26 08:38:30 -0400

    recent games mobile view

--- src/app/MatchLink.tsx
+++ src/app/MatchLink.tsx
@@ -9,34 +9,30 @@ export function MatchLink({ game }: { game: MatchModel }) {
   return (
     <Link
       href={`/live/${game.id}`}
-      className="p-1 bg-white border border-white hover:border-blue-700 flex justify-between hover:bg-blue-100 rounded"
+      className="p-1 bg-white border border-white hover:border-blue-700 flex justify-between hover:bg-blue-100 rounded flex-col sm:flex-row"
     >
-      <div className="tracking-tight">
-        <div className="flex gap-1 items-center">
-          <div
-            className={classNames(
-              game.player1 === game.winner && "text-blue-800"
-            )}
-          >
-            {game.player1}
-          </div>{" "}
-          <span className="text-xs">({game.player1Elo})</span>
-          <span className="text-gray-500"> vs.</span>
-          <div
-            className={classNames(
-              game.player2 === game.winner && "text-blue-800"
-            )}
-          >
-            {game.player2}
-          </div>{" "}
-          <span className="text-xs">({game.player2Elo})</span>
-        </div>
+      <div className="flex gap-1 items-center">
+        <div
+          className={classNames(
+            game.player1 === game.winner && "text-blue-800"
+          )}
+        >
+          {game.player1}
+        </div>{" "}
+        <span className="text-xs">({game.player1Elo})</span>
+        <span className="text-gray-500"> vs.</span>
+        <div
+          className={classNames(
+            game.player2 === game.winner && "text-blue-800"
+          )}
+        >
+          {game.player2}
+        </div>{" "}
+        <span className="text-xs">({game.player2Elo})</span>
       </div>
 
-      <div className="flex gap-1 items-center text-xs">
-        <div className="text-gray-600">
-          {DateTime.fromISO(game.createdAt).toRelative()}
-        </div>
+      <div className="flex gap-1 items-center text-xs text-gray-600">
+        {DateTime.fromISO(game.createdAt).toRelative()}
       </div>
     </Link>
   );
