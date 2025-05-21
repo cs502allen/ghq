0482b1ada7e4f3fc8bccddf4b07482095b407f8d
R. Tyler Julian <trjulian@gmail.com>
2025-05-21 06:31:34 -0400

    fix issue with too many prepended game numbers in page title

--- src/hooks/usePageTitle.ts
+++ src/hooks/usePageTitle.ts
@@ -1,10 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export function usePageTitle(title: string) {
+  const originalTitle = useRef<string>(document.title);
+
   useEffect(() => {
     if (title) {
-      const baseTitle = document.title;
-      document.title = `(${title}) ${baseTitle}`;
+      document.title = `(${title}) ${originalTitle.current}`;
+    } else {
+      document.title = originalTitle.current;
     }
+
+    return () => {
+      document.title = originalTitle.current;
+    };
   }, [title]);
 }
